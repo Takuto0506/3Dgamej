2f1fde32021457063b97a27ad49bca601505ca78
Takuto0506 <kamabokoendo@gmail.com>
2023-05-20 16:32:19 +0900

    JumpForce

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    [SerializeField] float gravityModifier;//重力値調整用
+    [SerializeField] float jumpForce;//ジャンプ力
+    [SerializeField] bool isOnGround;//地面についているか
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //スペースキーが押されて、かつ、地面にいたら
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            //上へ力を加える
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;//ジャンプした=地面にいない
+        }
+    }
+    //衝突が起きたら実行
+    private void OnCollisionEnter(Collision collision)
+    {
+        //ぶつかった相手（collsion）のタグがGroundなら
+        if(collision.gameObject.CompareTag("Ground"))
+        { 
+            isOnGround = true;//地面についている状態に変更
+        }
     }
 }
